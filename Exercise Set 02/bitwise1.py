525fee5c8546fcdce7d030bb824c05f732c64072
Primong <gayetadiona@gmail.com>
2024-08-28 12:00:28 +0800

    Add files via upload
    Exercise Set 02 (Aug 28, 2024)

---
+++ Exercise Set 02/bitwise1.py
@@ -0,0 +1,19 @@
+a = 12
+b = 25
+c = 21
+d = 27
+e = 8
+f = 7
+g = 13
+
+result1 = a | b
+result2 = c & d
+result3 = e ^ f
+result4 = g & f
+result5 = g | f
+
+print(f"a. {a} | {b}")
+print(f"b. {c} & {d}")
+print(f"c. {e} ^ {f}")
+print(f"d. {g} & {f}")
+print(f"e. {g} | {f}")
---
+++ Exercise Set 02/typecasting.py
@@ -0,0 +1,8 @@
+integer_value = int(input("Enter an integer: "))
+
+float_value = float(input("Enter a float: "))
+
+converted_integer = float(integer_value)
+difference = converted_integer - float_value
+
+print(f"Difference: {difference:.2f}")
